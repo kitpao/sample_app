3045aff64c1b51fa34c847586edc0e5bc03654a2
kitpao <kitpao91@gmail.com>
2020-07-14 17:26:05 +0000

    Update test to work without provide

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -8,7 +8,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "Ruby on Rails Sample App"
   end
 
   test "should get help" do
@@ -26,6 +26,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
     get root_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "Ruby on Rails Sample App"
   end
 end

ba14db593eb191a2bd56386c4ce680581fce4d2e
kitpao <kitpao91@gmail.com>
2020-07-14 17:16:18 +0000

    Use helper method to decide the title of the page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #helper to decide the use of | in page title
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Sample App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
